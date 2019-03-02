4fda970a9ea51235479b462c332bebda9c1e83c6
LAPTOP-RKNG9MQB\heyhe <heyheyforhello@outlook.com>
2019-03-01 19:42:47 -0500

    Template done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="mask">
+      <div id="login">
+        <login-board></login-board>
+      </div>
+    </div>
+
+    <!-- <img src="./assets/giphy.gif" alt=""> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import LoginBoard from './components/LoginBoard';
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    LoginBoard,
+  },
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
+#mask {
+  background-color: rgba(0, 0, 0, 0.5);
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  width: 100%;
+  /* Center and scale the image nicely */
+  background-image: url("./assets/giphy.gif");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+#login {
+  color: white;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
+
+
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'popper.js';
+import 'jquery';
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
